838d397511b696d893344c877272dfcb7b4ba78a
barti03 <barmalanczuk@gmail.com>
2014-05-05 22:07:28 +0200

    [#30] create model address with relation to shooter

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,17 @@ ActiveRecord::Schema.define(version: 20140508111951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "addresses", force: true do |t|
+    t.string   "street"
+    t.string   "city"
+    t.string   "building"
+    t.integer  "flat"
+    t.integer  "shooter_id"
+    t.string   "zip_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "competitions", force: true do |t|
     t.string   "name"
     t.string   "type"
