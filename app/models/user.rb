50f23332f97aec5a8cdba934214dd1af96afe0db
MatUrbanski <mateuszurbanski@yahoo.pl>
2014-05-06 11:54:49 +0200

    [#29] Add basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  
+  validates :password, presence: true, confirmation: true, length: {within: 6..40}
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, 
+                    uniqueness: true,                    
+                    format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+end
