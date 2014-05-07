a4c4336545233c0c2a4a9eaee98115d80d4e696f
Stanisław Tuszyński <stanislaw.tuszynski@inf.ug.edu.pl>
2014-05-07 10:40:56 +0200

    Adding controller for handling generation of decuments in PDF format filled with empty methods for each document.
    [#85]

---
+++ app/controllers/pdf_controller.rb
@@ -0,0 +1,15 @@
+class PdfController < ApplicationController
+
+  def competition_statement
+    #Some logic processing params[:competition]
+  end
+
+  def group_license_renewal
+    #Some logic
+  end
+
+  def license_renewal
+    #Some logic processing params[:shooter]
+  end
+end
+
