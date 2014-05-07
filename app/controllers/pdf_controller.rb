5227257c39789ddd599f1d74aeb0d487775b4c84
Stanisław Tuszyński <stanislaw.tuszynski@inf.ug.edu.pl>
2014-05-07 11:01:03 +0200

    Removing methods
    [#85]

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,15 +1,3 @@
 class PdfController < ApplicationController
-
-  def competition_statement
-    #Some logic processing params[:competition]
-  end
-
-  def group_license_renewal
-    #Some logic
-  end
-
-  def license_renewal
-    #Some logic processing params[:shooter]
-  end
 end
 
