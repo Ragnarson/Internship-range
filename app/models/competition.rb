53858f34f02b82b9fa5869b4741c9b77e870d3e1
Mateusz <matgrotha@gmail.com>
2014-05-08 11:51:05 +0200

    fixed indetation

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,7 +2,7 @@ class Competition < ActiveRecord::Base
   validates :name, presence: true
   validates :type, presence: true
 
-	def new
-		@competition = Competition.new
-	end
+  def new
+    @competition = Competition.new
+  end
 end
