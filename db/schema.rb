cca86503a5302ac3bbe0d5d5421f8e4c430520db
Fredar <a.krzeminski.freda@gmail.com>
2014-05-08 09:56:32 +0200

    Added updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507185630) do
+ActiveRecord::Schema.define(version: 20140508071555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "competitions", force: true do |t|
+    t.string   "name"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contests", force: true do |t|
     t.string   "name"
