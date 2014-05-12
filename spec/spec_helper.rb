79e0c6a684921ee899d2b8d7ad004d509067d064
Oskar Szrajer <oskarszrajer@gmail.com>
2014-05-12 22:34:15 +0200

    force new rspec syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
+  config.syntax = :expect
   config.order = "random"
 end
