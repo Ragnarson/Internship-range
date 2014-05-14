cfc8211bd4ecee4adf1c48633c20d06f6b0a8fc3
Michał Marcinkowski <michal.marcinkovski@gmail.com>
2014-05-14 09:34:36 +0200

    138-fix-competitions-form

---
+++ spec/features/new_competition_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe "View of new competition" do
+  subject { page }
+  let!(:contest) { create(:contest) }
+  before(:each) do
+    visit new_contest_competition_path(contest)
+  end
+
+  it "contains a headline" do
+    expect(page).to have_selector(
+      'h2', text: I18n.t('activerecord.models.competition.one').capitalize)
+  end
+
+  describe "labels" do
+    it "have field for comeptition name" do
+      expect(page).to have_field(
+        I18n.t('activerecord.attributes.competition.name'))
+    end
+
+    it "have field for classification" do
+      expect(page).to have_field(
+        I18n.t('activerecord.attributes.competition.classification'))
+    end
+
+    it "have field for number of shots" do
+      expect(page).to have_field(
+        I18n.t('activerecord.attributes.competition.number_of_shots'))
+    end
+  end
+end
+
