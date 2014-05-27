2e71ddb5875ab01d4a90ad065ff49c324fd6502b
Artur Krzemiński - Freda <a.krzeminski.freda@gmail.com>
2014-05-27 13:16:25 +0200

    [#268] Add new view for archived contests(that already took place) Add tests for new view, controller action Add controller action, route Fix tests that were inputting past date for future contests

--- spec/features/new_contest_spec.rb
+++ spec/features/new_contest_spec.rb
@@ -12,14 +12,14 @@ feature 'Contest' do
     fill_in I18n.t('activerecord.attributes.contest.name'),
     with: 'strzał'
     fill_in I18n.t('activerecord.attributes.contest.date'),
-    with: '2014-04-06'
+    with: Date.today + 1
 
     click_button I18n.t('helpers.submit.create', model: I18n.t('activerecord.models.contest'))
 
     expect(current_path).to eq contests_path
     expect(page).to have_content I18n.t('flash.success_create', model: I18n.t('activerecord.models.contest'))
 
-    expect(page).to have_content '2014-04-06'
+    expect(page).to have_content Date.today + 1
     expect(page).to have_content 'strzał'
   end
 end
