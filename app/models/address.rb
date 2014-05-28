155258e5465ecaced7f9270dca7a185a83149798
Maciej Kubiak <maciej.kubiak.net@gmail.com>
2014-05-28 14:44:06 +0200

    #292-adding-zip-code-validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ActiveRecord::Base
   validates :city, presence: true
   validates :building, presence: true
-  validates :zip_code, presence: true
+  validates :zip_code, presence: true, format: { with: /\A\d{2}-\d{3}\z/ }
   validates :shooter, presence: true
   belongs_to :shooter
 
